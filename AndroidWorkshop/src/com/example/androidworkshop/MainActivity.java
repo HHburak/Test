eee21367b685b86e8d5b7fdfb0e030c94ebe1798
Burak Oezfalci <HHburak@informatik.uni-hamburg.de>
2013-11-29 15:28:03 +0100

    safaffa

--- AndroidWorkshop/src/com/example/androidworkshop/MainActivity.java
+++ AndroidWorkshop/src/com/example/androidworkshop/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.androidworkshop;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-//Bffas
+//Bffasfassg
 public class MainActivity extends Activity {
 
 	@Override
